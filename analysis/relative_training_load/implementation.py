10d46d07d7fb3029fe6f7f7b7f4f9340740c8e2b
Urh Peček <pecek.urh@gmail.com>
2025-09-09 20:49:44 +0200

    updates

--- analysis/relative_training_load/implementation.py
+++ analysis/relative_training_load/implementation.py
@@ -2,15 +2,20 @@
 # Libraries
 # -----------------------------------------------------
 
+import os
+import sys
+repo_root = os.path.abspath(os.getcwd())  
+if repo_root not in sys.path:
+    sys.path.insert(0, repo_root)
+
 import gspread
 
-import os
-os.chdir("..")  
-print(f"Current wd = {os.getcwd()}")
 from src import config
 from src import help_functions as hf
 
-# googleDrive_client = gspread.authorize(config.DRIVE_CREDENTIALS)
-# training_data, _ = hf.import_google_sheet(googleDrive_client=googleDrive_client, filename=config.DRIVE_TP_LOG_FILENAMES[0], sheet_index=1)
+# Get Training data
+googleDrive_client = gspread.authorize(config.DRIVE_CREDENTIALS)
+training_data, _ = hf.import_google_sheet(googleDrive_client=googleDrive_client, filename=config.DRIVE_TP_LOG_FILENAMES[0], sheet_index=0)
+hastrl_data, _ = hf.import_google_sheet(googleDrive_client=googleDrive_client, filename=config.DRIVE_TP_LOG_FILENAMES[0], sheet_index=1)
 
 print("AA")
